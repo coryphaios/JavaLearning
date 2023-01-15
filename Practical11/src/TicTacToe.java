ff8dfdee692ed9d3599c2b7bd6131d44c3dfcad0
Igor Balakshev <balakshev@gmail.com>
2023-01-15 12:42:42 +0300

    TicTacToe outline

---
+++ Practical11/src/TicTacToe.java
@@ -0,0 +1,27 @@
+public class TicTacToe {
+    private char[][] field;
+
+    public TicTacToe(char[][] field) {
+        this.field = field;
+    }
+
+    public void newGame(){
+
+    }
+
+    public char[][] getField(){
+        char[][] field = new char[][]{{'-', '-', '-'}, {'-', '-', '-'}, {'-', '-', '-'}};
+        return field;
+    }
+
+    public String checkGame(){
+        String result = null;
+
+        return result;
+    }
+
+    public String makeMove(int x, int y){
+
+        return result;
+    }
+}
