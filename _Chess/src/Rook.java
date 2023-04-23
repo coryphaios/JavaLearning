598201807a5e3230536d1f77d0099171364260f2
Igor Balakshev <balakshev@gmail.com>
2023-04-23 22:44:41 +0300

    Rook can capture and can't move through another piece

--- _Chess/src/Rook.java
+++ _Chess/src/Rook.java
@@ -11,19 +11,51 @@ public class Rook extends ChessPiece {
 
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
 
-        if (toLine >= 0 && toLine <= 7){
-            if (toColumn >= 0 && toColumn <= 7){
-                if (((toLine != line) && (toColumn == column)) || ((toLine == line) && (toColumn != column))) {
-                    return true;
+        if (toLine >= 0 && toLine <= 7){ // Lines limit check
+            if (toColumn >= 0 && toColumn <= 7){ // columns limit check
+
+                if ((chessBoard.board[toLine][toColumn] == null) || !((chessBoard.board[toLine][toColumn].getColor()).equals(chessBoard.board[line][column].getColor()))){
+
+                    if (((toLine != line) && (toColumn == column)) || ((toLine == line) && (toColumn != column))) { // possible directions check
+
+                        if (toLine == line && toColumn > column) { //condition for right direction on the same line
+                            for (int i = column + 1; i < toColumn; i++) {
+                                if (chessBoard.board[line][i] != null) { // free square check
+                                    return false;
+                                }
+                            }
+                            return true;
+                        } else if (toLine == line && toColumn < column) { //condition for left direction on the same line
+                            for (int i = column - 1; i > toColumn; i--) {
+                                if (chessBoard.board[line][i] != null) {
+                                    return false;
+                                }
+                            }
+                            return true;
+                        } else if (toColumn == column && toLine > line) { //condition for forward direction on the same column
+                            for (int i = line + 1; i < toLine; i++) {
+                                if (chessBoard.board[i][column] != null) {
+                                    return false;
+                                }
+                            }
+                            return true;
+                        } else if (toColumn == column && toLine < line) { //condition for backward direction on the same column
+                            for (int i = line - 1; i > toLine; i--) {
+                                if (chessBoard.board[i][column] != null) {
+                                    return false;
+                                }
+                            }
+                            return true;
+                        }
+                        return false;
+                    }
+                    return false;
                 }
                 return false;
             }
             return false;
         }
-
-        else {
-            return false;
-        }
+        return false;
     }
 
     public String getSymbol() {
