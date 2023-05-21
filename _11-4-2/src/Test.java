2676c34b38b285db18912eaea173e50d4375b7db
Igor Balakshev <balakshev@gmail.com>
2023-05-21 18:05:17 +0300

    11 module, some tasks

---
+++ _11-4-2/src/Test.java
@@ -0,0 +1,17 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Test {
+    public static void main(String[] args) {
+        try {
+            readFromFile();
+        } catch (IOException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.print("Done");
+    }
+
+    public static void readFromFile() throws IOException {
+        System.out.print("Reading line ");
+    }
+}
---
+++ _11-4-3/src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static String someMethod() {
+        try {
+            throw new NullPointerException();
+        } catch (NullPointerException e) {
+            return "catch";
+        } finally {
+            return "finally";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(someMethod());
+    }
+}
